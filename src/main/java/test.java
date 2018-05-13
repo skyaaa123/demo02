db8ae17acb0e1aa66a7991319f8d4f900c0daca4
skyaaa123 <1143368663@qq.com>
2018-05-13 11:26:11 +0800

    demo10

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -5,5 +5,6 @@ public class test {
     public static void main(String []args) {
             System.out.println("test01");
             System.out.println("test02");
+            System.out.println("demo10");//demo10第一次test
     }
 }
